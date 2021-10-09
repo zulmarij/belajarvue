438e544616393716d4b20dc65c913976fe5d99df
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2021-10-09 16:22:38 +0700

    start to projek

---
+++ hello-world/src/components/Logos.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="logos-wrapper">
+        <h2 class="title-header">Find Best Logo</h2>
+        <div class="item-logos">
+              <img class="logo" alt="Vue logo" src="../assets/logo.png">
+        </div>
+        <div class="item-logos">
+              <img class="logo" alt="Vue logo" src="../assets/logo.png">
+        </div>
+        <div class="item-logos">
+              <img class="logo" alt="Vue logo" src="../assets/logo.png">
+        </div>
+        <div class="item-logos">
+              <img class="logo" alt="Vue logo" src="../assets/logo.png">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Logos'
+}
+</script>
+
+<style scoped>
+.title-header {
+    margin-bottom: 80px;
+    text-align: left;
+}
+
+.logos-wrapper {
+    width: 66%;
+    overflow: hidden;
+    margin: auto;
+}
+
+.item-logos {
+    float: left;
+    margin-right: 70px;
+    margin-bottom: 70px;
+    width: 150px;
+    height: 150px;
+}
+
+.item-logos .logo {
+    width: 150px;
+    height: 150px;
+}
+</style>
