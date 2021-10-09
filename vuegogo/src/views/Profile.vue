4454787429ceaac1cac5977c050019beca4053f5
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2021-10-09 17:03:57 +0700

    Manager

---
+++ vuegogo/src/views/Profile.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="profile">
+    <h1>This is an profile page</h1>
+  </div>
+</template>
+
+<script>
+
+import 'bootstrap/dist/css/bootstrap.css'
+
+export default {
+  name: 'Profile',
+  components: {
+
+  }
+}
+</script>
